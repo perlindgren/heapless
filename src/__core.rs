3a9bbdce5bee40fdc60fe7609ed2992b2b117138
Per <Per Lindgren>
2019-05-05 19:04:16 +0200

    Option based MaybeUninit

--- src/__core.rs
+++ src/__core.rs
@@ -15,7 +15,7 @@ pub mod mem {
     // workaround to get this to compile on stable ("unions with non-`Copy` fields are unstable")
     #[cfg(not(feature = "const-fn"))]
     pub struct MaybeUninit<T> {
-        value: ManuallyDrop<T>,
+        value: Option<T>,
     }
 
     impl<T> MaybeUninit<T> {
@@ -27,7 +27,7 @@ pub mod mem {
         #[cfg(not(feature = "const-fn"))]
         pub unsafe fn uninitialized() -> Self {
             MaybeUninit {
-                value: ManuallyDrop::new(mem::uninitialized()),
+                value: None,
             }
         }
 
@@ -37,18 +37,82 @@ pub mod mem {
         ///
         /// It is up to the caller to guarantee that the the `MaybeUninit` really is in an
         /// initialized state, otherwise this will immediately cause undefined behavior.
+        #[cfg(feature = "const-fn")]
         pub unsafe fn get_ref(&self) -> &T {
             &*self.value
         }
 
+        #[cfg(not(feature = "const-fn"))]
+        pub unsafe fn get_ref(&self) -> &T {
+            &self.value.as_ref().unwrap()
+        }
         /// Get a mutable reference to the contained value.
         ///
         /// # Unsafety
         ///
         /// It is up to the caller to guarantee that the the `MaybeUninit` really is in an
         /// initialized state, otherwise this will immediately cause undefined behavior.
+        #[cfg(feature = "const-fn")]
         pub unsafe fn get_mut(&mut self) -> &mut T {
             &mut *self.value
         }
+
+        #[cfg(not(feature = "const-fn"))]
+        pub unsafe fn get_mut(&mut self) -> &mut T {
+            self.value.as_mut().unwrap()
+        }
     }
 }
+
+// pub mod mem {
+//     #[cfg(not(feature = "const-fn"))]
+//     pub use core::mem;
+//     pub use core::mem::{replace, zeroed, ManuallyDrop};
+
+//     // See RFC 1892
+//     #[cfg(feature = "const-fn")]
+//     pub union MaybeUninit<T> {
+//         uninit: (),
+//         value: ManuallyDrop<T>,
+//     }
+
+//     // workaround to get this to compile on stable ("unions with non-`Copy` fields are unstable")
+//     #[cfg(not(feature = "const-fn"))]
+//     pub struct MaybeUninit<T> {
+//         value: ManuallyDrop<T>,
+//     }
+
+//     impl<T> MaybeUninit<T> {
+//         #[cfg(feature = "const-fn")]
+//         pub const unsafe fn uninitialized() -> Self {
+//             MaybeUninit { uninit: () }
+//         }
+
+//         #[cfg(not(feature = "const-fn"))]
+//         pub unsafe fn uninitialized() -> Self {
+//             MaybeUninit {
+//                 value: ManuallyDrop::new(mem::uninitialized()),
+//             }
+//         }
+
+//         /// Get a reference to the contained value.
+//         ///
+//         /// # Unsafety
+//         ///
+//         /// It is up to the caller to guarantee that the the `MaybeUninit` really is in an
+//         /// initialized state, otherwise this will immediately cause undefined behavior.
+//         pub unsafe fn get_ref(&self) -> &T {
+//             &*self.value
+//         }
+
+//         /// Get a mutable reference to the contained value.
+//         ///
+//         /// # Unsafety
+//         ///
+//         /// It is up to the caller to guarantee that the the `MaybeUninit` really is in an
+//         /// initialized state, otherwise this will immediately cause undefined behavior.
+//         pub unsafe fn get_mut(&mut self) -> &mut T {
+//             &mut *self.value
+//         }
+//     }
+// }
